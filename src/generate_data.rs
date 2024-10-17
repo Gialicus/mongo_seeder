0b980d9dce61ac37746c1a67b928537dc581d3ab
giali.coletta <51245135+Gialicus@users.noreply.github.com>
2024-10-17 17:56:39 +0200

    feat(generate_data): add more faker case

--- src/generate_data.rs
+++ src/generate_data.rs
@@ -1,7 +1,12 @@
 use fake::faker::address::raw::*;
+use fake::faker::boolean::raw::*;
 use fake::faker::company::raw::*;
+use fake::faker::creditcard::raw::*;
 use fake::faker::currency::raw::*;
+use fake::faker::filesystem::raw::*;
 use fake::faker::internet::raw::*;
+use fake::faker::job::raw::{Title as JobTitle, *};
+use fake::faker::lorem::raw::*;
 use fake::faker::name::raw::*;
 use fake::Fake;
 use mongodb::bson::oid::ObjectId;
@@ -16,6 +21,171 @@ fn generate_fake_value(
     ids_pool: &HashMap<String, Vec<ObjectId>>,
 ) -> mongodb::bson::Bson {
     match fake_method {
+        // Address
+        "fake.address.buildingNumber" => {
+            mongodb::bson::Bson::String(BuildingNumber(fake::locales::EN).fake::<String>())
+        }
+        "fake.address.cityName" => {
+            mongodb::bson::Bson::String(CityName(fake::locales::EN).fake::<String>())
+        }
+        "fake.address.cityPrefix" => {
+            mongodb::bson::Bson::String(CityPrefix(fake::locales::EN).fake::<String>())
+        }
+        "fake.address.citySuffix" => {
+            mongodb::bson::Bson::String(CitySuffix(fake::locales::EN).fake::<String>())
+        }
+        "fake.address.countryCode" => {
+            mongodb::bson::Bson::String(CountryCode(fake::locales::EN).fake::<String>())
+        }
+        "fake.address.countryName" => {
+            mongodb::bson::Bson::String(CountryName(fake::locales::EN).fake::<String>())
+        }
+        "fake.address.latitude" => {
+            mongodb::bson::Bson::String(Latitude(fake::locales::EN).fake::<String>())
+        }
+        "fake.address.longitude" => {
+            mongodb::bson::Bson::String(Longitude(fake::locales::EN).fake::<String>())
+        }
+        "fake.address.postCode" => {
+            mongodb::bson::Bson::String(PostCode(fake::locales::EN).fake::<String>())
+        }
+        "fake.address.secondaryAddress" => {
+            mongodb::bson::Bson::String(SecondaryAddress(fake::locales::EN).fake::<String>())
+        }
+        "fake.address.stateAbbr" => {
+            mongodb::bson::Bson::String(StateAbbr(fake::locales::EN).fake::<String>())
+        }
+        "fake.address.stateName" => {
+            mongodb::bson::Bson::String(StateName(fake::locales::EN).fake::<String>())
+        }
+        "fake.address.streetName" => {
+            mongodb::bson::Bson::String(StreetName(fake::locales::EN).fake::<String>())
+        }
+        "fake.address.streetSuffix" => {
+            mongodb::bson::Bson::String(StreetSuffix(fake::locales::EN).fake::<String>())
+        }
+        "fake.address.timeZone" => {
+            mongodb::bson::Bson::String(TimeZone(fake::locales::EN).fake::<String>())
+        }
+        "fake.address.zipCode" => {
+            mongodb::bson::Bson::String(ZipCode(fake::locales::EN).fake::<String>())
+        }
+
+        // Boolean
+        "fake.boolean.boolean" => {
+            mongodb::bson::Bson::Boolean(Boolean(fake::locales::EN, 1).fake())
+        }
+
+        // Chrono
+        "fake.chrono.date" => mongodb::bson::Bson::String(
+            fake::faker::chrono::raw::Date(fake::locales::EN).fake::<String>(),
+        ),
+        "fake.chrono.datetime" => mongodb::bson::Bson::String(
+            fake::faker::chrono::raw::DateTime(fake::locales::EN).fake::<String>(),
+        ),
+        "fake.chrono.duration" => mongodb::bson::Bson::String(
+            fake::faker::number::raw::NumberWithFormat(fake::locales::EN, "###").fake::<String>(),
+        ),
+        "fake.chrono.time" => mongodb::bson::Bson::String(
+            fake::faker::chrono::raw::Time(fake::locales::EN).fake::<String>(),
+        ),
+
+        // Company
+        "fake.company.bs" => mongodb::bson::Bson::String(Bs(fake::locales::EN).fake::<String>()),
+        "fake.company.buzzword" => {
+            mongodb::bson::Bson::String(Buzzword(fake::locales::EN).fake::<String>())
+        }
+        "fake.company.catchPhrase" => {
+            mongodb::bson::Bson::String(CatchPhrase(fake::locales::EN).fake::<String>())
+        }
+        "fake.company.name" => {
+            mongodb::bson::Bson::String(CompanyName(fake::locales::EN).fake::<String>())
+        }
+        "fake.company.profession" => {
+            mongodb::bson::Bson::String(Profession(fake::locales::EN).fake::<String>())
+        }
+        "fake.company.suffix" => {
+            mongodb::bson::Bson::String(Suffix(fake::locales::EN).fake::<String>())
+        }
+        "fake.company.industry" => {
+            mongodb::bson::Bson::String(Industry(fake::locales::EN).fake::<String>())
+        }
+
+        // CreditCard
+        "fake.creditCard.number" => {
+            mongodb::bson::Bson::String(CreditCardNumber(fake::locales::EN).fake::<String>())
+        }
+
+        // Currency
+        "fake.currency.code" => {
+            mongodb::bson::Bson::String(CurrencyCode(fake::locales::EN).fake::<String>())
+        }
+        "fake.currency.name" => {
+            mongodb::bson::Bson::String(CurrencyName(fake::locales::EN).fake::<String>())
+        }
+        "fake.currency.symbol" => {
+            mongodb::bson::Bson::String(CurrencySymbol(fake::locales::EN).fake::<String>())
+        }
+
+        // FileSystem
+        "fake.fileSystem.extension" => {
+            mongodb::bson::Bson::String(FileExtension(fake::locales::EN).fake::<String>())
+        }
+        "fake.fileSystem.fileName" => {
+            mongodb::bson::Bson::String(FileName(fake::locales::EN).fake::<String>())
+        }
+        "fake.fileSystem.filePath" => {
+            mongodb::bson::Bson::String(FilePath(fake::locales::EN).fake::<String>())
+        }
+        "fake.fileSystem.mimeType" => {
+            mongodb::bson::Bson::String(MimeType(fake::locales::EN).fake::<String>())
+        }
+
+        // Internet
+        "fake.internet.domainSuffix" => {
+            mongodb::bson::Bson::String(DomainSuffix(fake::locales::EN).fake::<String>())
+        }
+        "fake.internet.freeEmail" => {
+            mongodb::bson::Bson::String(FreeEmail(fake::locales::EN).fake::<String>())
+        }
+        "fake.internet.ipV4" => {
+            mongodb::bson::Bson::String(IPv4(fake::locales::EN).fake::<String>())
+        }
+        "fake.internet.ipV6" => {
+            mongodb::bson::Bson::String(IPv6(fake::locales::EN).fake::<String>())
+        }
+        "fake.internet.password" => {
+            mongodb::bson::Bson::String(Password(fake::locales::EN, 8..12).fake::<String>())
+        }
+        "fake.internet.email" => {
+            mongodb::bson::Bson::String(FreeEmail(fake::locales::EN).fake::<String>())
+        }
+        "fake.internet.username" => {
+            mongodb::bson::Bson::String(Username(fake::locales::EN).fake::<String>())
+        }
+
+        // Job
+        "fake.job.field" => mongodb::bson::Bson::String(Field(fake::locales::EN).fake::<String>()),
+        "fake.job.position" => {
+            mongodb::bson::Bson::String(Position(fake::locales::EN).fake::<String>())
+        }
+        "fake.job.seniority" => {
+            mongodb::bson::Bson::String(Seniority(fake::locales::EN).fake::<String>())
+        }
+        "fake.job.title" => {
+            mongodb::bson::Bson::String(JobTitle(fake::locales::EN).fake::<String>())
+        }
+
+        // Lorem
+        "fake.lorem.sentence" => {
+            mongodb::bson::Bson::String(Sentence(fake::locales::EN, 1..5).fake::<String>())
+        }
+        "fake.lorem.word" => mongodb::bson::Bson::String(Word(fake::locales::EN).fake::<String>()),
+        "fake.lorem.paragraph" => {
+            mongodb::bson::Bson::String(Paragraph(fake::locales::EN, 1..5).fake::<String>())
+        }
+
+        // Name
         "fake.name.firstName" => {
             mongodb::bson::Bson::String(FirstName(fake::locales::EN).fake::<String>())
         }
@@ -28,54 +198,10 @@ fn generate_fake_value(
             LastName(fake::locales::EN).fake::<String>()
         )),
 
-        "fake.address.streetName" => {
-            mongodb::bson::Bson::String(StreetName(fake::locales::EN).fake::<String>())
-        }
-        "fake.address.city" => {
-            mongodb::bson::Bson::String(CityName(fake::locales::EN).fake::<String>())
-        }
-        "fake.address.zipCode" => {
-            mongodb::bson::Bson::String(ZipCode(fake::locales::EN).fake::<String>())
-        }
-
-        "fake.internet.email" => {
-            mongodb::bson::Bson::String(FreeEmail(fake::locales::EN).fake::<String>())
-        }
-        "fake.internet.username" => {
-            mongodb::bson::Bson::String(Username(fake::locales::EN).fake::<String>())
-        }
-
-        "fake.company.name" => {
-            mongodb::bson::Bson::String(CompanyName(fake::locales::EN).fake::<String>())
-        }
-        "fake.company.suffix" => {
-            mongodb::bson::Bson::String(CompanySuffix(fake::locales::EN).fake::<String>())
-        }
-
         "fake.number.u8" => mongodb::bson::Bson::Int32((0..=255).fake::<u8>() as i32),
         "fake.number.i32" => mongodb::bson::Bson::Int32((0..=1000).fake::<i32>()),
         "fake.number.u32" => mongodb::bson::Bson::Int64((0..=1000).fake::<u32>() as i64),
         "fake.number.i64" => mongodb::bson::Bson::Int64((0..=1000).fake::<i64>()),
-
-        "fake.currency.code" => {
-            mongodb::bson::Bson::String(CurrencyCode(fake::locales::EN).fake::<String>())
-        }
-        "fake.currency.name" => {
-            mongodb::bson::Bson::String(CurrencyName(fake::locales::EN).fake::<String>())
-        }
-        "fake.currency.symbol" => {
-            mongodb::bson::Bson::String(CurrencySymbol(fake::locales::EN).fake::<String>())
-        }
-
-        "fake.chrono.datetime" => mongodb::bson::Bson::String(
-            fake::faker::chrono::raw::DateTime(fake::locales::EN).fake::<String>(),
-        ),
-        "fake.chrono.date" => mongodb::bson::Bson::String(
-            fake::faker::chrono::raw::Date(fake::locales::EN).fake::<String>(),
-        ),
-        "fake.chrono.duration" => mongodb::bson::Bson::String(
-            fake::faker::number::raw::NumberWithFormat(fake::locales::EN, "###").fake::<String>(),
-        ),
 
         "fake.random.uuid" => mongodb::bson::Bson::String(fake::uuid::UUIDv4.fake::<String>()),
 
@@ -84,6 +210,13 @@ fn generate_fake_value(
             let collection_id = ids_pool.get(collection_name).unwrap();
             let random_id = collection_id.choose(&mut rand::thread_rng()).unwrap();
             mongodb::bson::Bson::ObjectId(*random_id)
+        }
+
+        method if fake_method.starts_with("from") => {
+            let sequence = method.split('.').collect::<Vec<&str>>()[1];
+            let values = sequence.split('|').collect::<Vec<&str>>();
+            let random_value = values.choose(&mut rand::thread_rng()).unwrap();
+            mongodb::bson::Bson::String(random_value.to_string())
         }
 
         _ => mongodb::bson::Bson::String("unsupported_method".to_string()),
